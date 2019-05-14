fdf3ba4783367f0f5fb05c78d3a22b43908976b5
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-05-14 11:12:59 -0700

    updated codes to provide products information and let customers to place order (challenge 1)

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,6 +35,74 @@ function displayInfo() {
             "|| Stock Quantity: " +
             res[i].stock_quantity
             );
+            item_idSearch();
     });
+  
 
+
+}
+
+
+function item_idSearch() {
+    inquirer
+    .prompt({
+        name: "item_id",
+        type: "input",
+        message: "What item id would you like to purchase?"
+
+    })
+    .then(function(answer){
+        var query = "SELECT * FROM products WHERE ?";
+        connection.query(query, {item_id: answer.item_id}, function(err, res){
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+                console.log("Item ID: " + 
+                res[i].item_id +
+                "|| Product Name: " + 
+                res[i].product_name +
+                "|| Department Name: " +
+                res[i].department_name +
+                "|| Price: " +
+                res[i].price +
+                "|| Stock Quantity: " +
+                res[i].stock_quantity
+                );
+            }
+        purchase_unitSearch(res[0]);   
+        })
+    })
+}
+
+function purchase_unitSearch(result) {
+    inquirer
+    .prompt({
+        name: "purchase_unit",
+        type: "input",
+        message: "How many units of the product would you like to purchase?"
+    })
+    .then(function(input){
+        console.log("\nI would like to buy " + input.purchase_unit);
+        stock_quantityCheck(result, input);
+    })
+}
+
+function stock_quantityCheck(result, input) {
+    console.log(input.purchase_unit);
+    console.log(result.stock_quantity);
+    
+        if (input.purchase_unit > result.stock_quantity) {
+            console.log("\nInsufficient quantity for item id: " + result.item_id)
+        }else {order_place(result, input);}
+        
+    
+}
+
+function order_place(result, input){
+    var newQuanity = result.stock_quantity - input.purchase_unit;
+    var query = "UPDATE products SET stock_quantity=? WHERE item_id =?";
+    connection.query(query, [newQuanity, result.item_id], function(err, res){
+        if(err) throw err;
+
+        console.log("The total price for the product " + result.item_id + " is " + input.purchase_unit * result.price);
+    });
 }
